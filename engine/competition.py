98bf933e95789a7ba70f1a09d31708e7b963e76c
kierancoz <kierancoz.w@gmail.com>
2022-09-29 14:44:16 -0500

    working autocross

--- engine/competition.py
+++ engine/competition.py
@@ -5,13 +5,15 @@ class Competition:
         self.racecar = racecar
 
         self.endurance_sim = engine.Simulation(endurance, self.racecar)
-        self.autocross_sim = autocross
+        self.autocross_sim = engine.Simulation(autocross, self.racecar)
         self.skidpad_sim = None
         self.acceleration_sim = None
 
     def run(self):
+        print("Running Endurance Sim")
         result_endurance, time_e = self.endurance_sim.run()
-        results_autocross, time_ax = None, None #Simulation(self.autocross, self.racecar).run()
+        print("Running Autocross Sim")
+        results_autocross, time_ax = self.autocross_sim.run()
         results_skidpad, time_s = None, None #Simulation(self.skidpad, self.racecar).run()
         results_acceleration, time_a = None, None #Simulation(self.acceleration, self.racecar).run()
         
@@ -21,6 +23,5 @@ class Competition:
         return [result_endurance, results_autocross, results_skidpad, results_acceleration], points, times
 
     def points(self, time_e, time_ax, time_s, time_a):
-        best = self.endurance_sim.track.best_time
-        worst = self.endurance_sim.track.worst_time
+        # TODO
         return 400
--- main.py
+++ main.py
@@ -6,31 +6,32 @@ def main():
     easy_driver = engine.Racecar("")
 
     endurance_track = engine.Track("endurance_michigan_2019", 120, 100)
-    autocross_track = None #engine.Track("autocross_michigan_2019.csv", 1, 1)
+    autocross_track = engine.Track("autocross_michigan_2019", 1, 1)
 
-    ### Example: Sweeping!
+    ### Example: Sweeping! ###
     
     cg_heights = [12,13,14]
     results_df = pd.DataFrame(columns=["cg_height","points", "endurance_time"])
 
-    for index, cg_height in enumerate(cg_heights):
-        easy_driver.params.cg_height = cg_height
-        easy_driver.regenerate_GGV()
+    # for index, cg_height in enumerate(cg_heights):
+    #     easy_driver.params.cg_height = cg_height
+    #     easy_driver.regenerate_GGV()
 
-        # If a new racing line is needed (i.e. trackwidth changes), enable these lines
-        #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
-        #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
+    #     # If a new racing line is needed (i.e. trackwidth changes), enable these lines
+    #     #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
+    #     #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
 
-        _, points, times = engine.Competition(endurance_track, autocross_track, easy_driver).run()
-        results_df.loc[index] = [cg_height, points, times[0]]
+    #     _, points, times = engine.Competition(endurance_track, autocross_track, easy_driver).run()
+    #     results_df.loc[index] = [cg_height, points, times[0]]
 
-    results_df.to_csv("results/cg_height_sweep-easy_driver.csv")
+    # results_df.to_csv("results/cg_height_sweep-easy_driver.csv")
 
-    ### Example: One off simulations
 
-    # results, points, times = engine.Competition(endurance_track, autocross_track, easy_driver).run()
-    # results[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
-    #results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
+    ### Example: One off simulations ###
+
+    results, points, times = engine.Competition(endurance_track, autocross_track, easy_driver).run()
+    results[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
+    results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
     
 
 if __name__ == "__main__":
