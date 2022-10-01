8b3297848459f9c43a26dc1e648ae938c804189a
kierancoz <kierancoz.w@gmail.com>
2022-10-01 11:47:44 -0500

    switch to metric

--- engine/racecar.py
+++ engine/racecar.py
@@ -6,20 +6,24 @@ class Racecar:
         self.max_vel = 29 # m / s
         self.params = types.SimpleNamespace()
         self.params.front_trackwidth = 1.2192
+        self.params.temp_accel_max = 1.2
+        self.params.temp_deccel_max = 1.8
+        self.params.temp_lateral_max = 1.75
+
 
     def regenerate_GGV(self):
         pass
 
     def accel(self, vel, lateral = 0):
-        accel_max = 1.1
+        accel_max = self.params.temp_accel_max
         return (accel_max**2 - lateral**2*accel_max**2/self.lateral(0)**2)**0.5
 
     def deccel(self, vel, lateral = 0):
-        deccel_max = 2
+        deccel_max = self.params.temp_deccel_max
         return (deccel_max**2 - lateral**2*deccel_max**2/self.lateral(0)**2)**0.5
 
     def lateral(self, vel):
-        return 1.7
+        return self.params.temp_lateral_max
 
     def max_vel_corner(self, radius):
         if radius > 1000 or radius == 0: # TODO
--- main.py
+++ main.py
@@ -12,17 +12,22 @@ def main():
 
     ### Example: Sweeping! ###
     
-    # cg_heights = [12,13,14]
+
+    # accel_gs = [1.2,1.1,1]
+    # #lat_gs = [1.8,1.9,2.0]
+    # #deccel_gs = [1.6,1.7,1.8]
     # results_df = pd.DataFrame(columns=["cg_height","points", "endurance_time"])
-    # for index, cg_height in enumerate(cg_heights):
-    #     easy_driver.params.cg_height = cg_height
+    # for index, accel in enumerate(accel_gs):
+    #     easy_driver.params.temp_accel_max = accel
     #     easy_driver.regenerate_GGV()
 
     #     # If a new racing line is needed (i.e. trackwidth changes), enable these lines
     #     #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
     #     #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
 
-    #     _, points, times = engine.Competition(endurance_track, autocross_track, easy_driver).run()
+    #     _, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
+    #                          skidpad_times, accel_times).run()
+    #     print(times)
     #     results_df.loc[index] = [cg_height, points, times[0]]
     # results_df.to_csv("results/cg_height_sweep-easy_driver.csv")
 
@@ -30,9 +35,9 @@ def main():
     ### Example: One-off simulations ###
 
     results, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
-                             skidpad_times, accel_times).run()
-    #esults[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
-    #results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
+                            skidpad_times, accel_times).run()
+    results[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
+    results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
     print(times)
     
 
