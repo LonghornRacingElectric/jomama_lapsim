0ead3898638fd58dc25932063793c165b2ae9b15
Terry Zhang <terryzm@utexas.edu>
2022-10-02 18:33:52 -0500

    add points function (again)

--- main.py
+++ main.py
@@ -5,29 +5,24 @@ import engine
 def main():
     easy_driver = engine.Racecar("")
 
-    endurance_track = engine.Track("endurance_michigan_2019", 1, 1)
-    autocross_track = engine.Track("autocross_michigan_2019", 1, 1)
-    skidpad_times = [1, 1]
-    accel_times = [1, 1]
+    endurance_track = engine.Track("endurance_michigan_2019", 1681.963, 2467.84635)
+    autocross_track = engine.Track("autocross_michigan_2019", 63.236, 91.6922)
+    skidpad_times = [5.0497, 6.1704]
+    accel_times = [4.004, 6.006]
 
     ### Example: Sweeping! ###
     
-
-    # accel_gs = [1.2,1.1,1]
-    # #lat_gs = [1.8,1.9,2.0]
-    # #deccel_gs = [1.6,1.7,1.8]
+    # cg_heights = [12,13,14]
     # results_df = pd.DataFrame(columns=["cg_height","points", "endurance_time"])
-    # for index, accel in enumerate(accel_gs):
-    #     easy_driver.params.temp_accel_max = accel
+    # for index, cg_height in enumerate(cg_heights):
+    #     easy_driver.params.cg_height = cg_height
     #     easy_driver.regenerate_GGV()
 
     #     # If a new racing line is needed (i.e. trackwidth changes), enable these lines
     #     #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
     #     #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
 
-    #     _, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
-    #                          skidpad_times, accel_times).run()
-    #     print(times)
+    #     _, points, times = engine.Competition(endurance_track, autocross_track, easy_driver).run()
     #     results_df.loc[index] = [cg_height, points, times[0]]
     # results_df.to_csv("results/cg_height_sweep-easy_driver.csv")
 
@@ -35,10 +30,12 @@ def main():
     ### Example: One-off simulations ###
 
     results, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
-                            skidpad_times, accel_times).run()
+                             skidpad_times, accel_times).run()
     results[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
     results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
     print(times)
+    print(points)
+
     
 
 if __name__ == "__main__":
