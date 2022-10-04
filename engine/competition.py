54234be2d5d8316cc64e5100eb517e5994771b33
terryzm1 <113315651+terryzm1@users.noreply.github.com>
2022-10-04 14:09:07 -0500

    add points function (#3)
    * add points function
    
    * add points function (again)
    
    * remove extra tmax
    
    * fix merge error

--- engine/competition.py
+++ engine/competition.py
@@ -4,8 +4,8 @@ class Competition:
     def __init__(self, racecar, endurance, autocross, skidpad_times = None, accel_times = None):
         self.racecar = racecar
 
-        accel_track = engine.Track("Acceleration", best_time = accel_times[0], worst_time = accel_times[1])
-        skidpad_track = engine.Track(best_time = skidpad_times[0], worst_time = skidpad_times[1])
+        accel_track = engine.Track("Acceleration", best_time = accel_times)
+        skidpad_track = engine.Track(best_time = skidpad_times)
         
         self.endurance_sim = engine.Simulation(self.racecar, endurance)
         self.autocross_sim = engine.Simulation(self.racecar, autocross)
@@ -28,5 +28,42 @@ class Competition:
         return [result_endurance, results_autocross, results_skidpad, results_acceleration], points, times
 
     def points(self, time_e, time_ax, time_s, time_a):
-        # TODO
-        return 400
+        a_min = self.acceleration_sim.track.best_time
+        a_max = 1.5 * a_min
+        if time_a < a_min:
+            score_a = 100
+        elif time_a <= a_max:
+            score_a = 95.5 * ((a_max/time_a - 1)/(a_max/a_min - 1)) + 4.5
+        else:
+            score_a = 4.5
+        
+        s_min = self.skidpad_sim.track.best_time
+        s_max = 1.25 * s_min
+        if time_s < s_min:
+            score_s = 75
+        elif time_s <= s_max:
+            score_s = 71.5 * (((s_max/time_s)**2 - 1)/((s_max/s_min)**2 - 1)) + 3.5
+        else:
+            score_s = 3.5
+        
+        ax_min = self.autocross_sim.track.best_time
+        ax_max = 1.45 * ax_min
+        if time_ax < ax_min:
+            score_ax = 125
+        elif time_ax <= ax_max:
+            score_ax = 118.5 * ((ax_max/time_ax - 1)/(ax_max/ax_min) - 1) + 6.5
+        else:
+            score_ax = 6.5
+
+        e_min = self.endurance_sim.track.best_time
+        e_max = 1.45 * e_min
+        time_e_total = 15 * time_e
+        if time_e_total < e_min:
+            score_e = 275
+        elif time_e_total <= e_max:
+            score_e = 250 * ((e_max/time_e_total - 1)/(e_max/e_min - 1)) + 25
+        else:
+            score_e = 25
+
+        return [score_e, score_ax, score_s, score_a]
+    
--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import engine
 def main():
     easy_driver = engine.Racecar("")
 
-    endurance_track = engine.Track("endurance_michigan_2019", 1, 1)
-    autocross_track = engine.Track("autocross_michigan_2019", 1, 1)
-    skidpad_times = [1, 1]
-    accel_times = [1, 1]
+    endurance_track = engine.Track("endurance_michigan_2019", 1681.963)
+    autocross_track = engine.Track("autocross_michigan_2019", 63.236)
+    skidpad_times = 5.0497
+    accel_times = 4.004
 
     ### Example: Sweeping! ###
     
-
+    
     # accel_gs = [1.2,1.1,1]
     # #lat_gs = [1.8,1.9,2.0]
     # #deccel_gs = [1.6,1.7,1.8]
@@ -35,10 +35,12 @@ def main():
     ### Example: One-off simulations ###
 
     results, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
-                            skidpad_times, accel_times).run()
+                             skidpad_times, accel_times).run()
     results[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
     results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
     print(times)
+    print(points)
+
     
 
 if __name__ == "__main__":
