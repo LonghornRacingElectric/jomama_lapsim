2836a13699afe6b4cb240584ea675cee2b7820b2
LHR-General <91154162+LHR-General@users.noreply.github.com>
2022-10-23 14:04:26 -0500

    Fixed sweeps

--- aero_sweeps.py
+++ aero_sweeps.py
@@ -4,14 +4,15 @@ import engine
 import engine.magic_moment_method.vehicle_params as vehicles
 import engine.magic_moment_method.state_solver.aerodynamics as Aero
 import engine.magic_moment_method.state_solver.logger as Fuck_this_Logger
+pd.options.mode.chained_assignment = None
 
 # TODO: Move this file into sweeping, gonna have to mess with module imports
 
 # Test Parameters #
 #######################################
 TEST_MIN = 0
-TEST_MAX = 4
-divisions = 3
+TEST_MAX = 6
+divisions = 5
 
 K = 0.06215178719838  # lift-induced drag constant
 Cl0 = 0  # lift at 0 induced drag
@@ -24,7 +25,7 @@ Cl_A = refA * Cl
 # TODO: Upgrade this to reflect the new fit from Andrew Zhang, basically Cl will increase energy consumption (kWh) due to drag
 #           then the extra kWh will correlate to more mass (Andrews equation). Challenge is to correlate drag and kWh.
 #               LapSim should be able to do so given the torque request. If you have questions about it contact Igor Souza
-Aero_mass = 0.3745 * (Cl_A ** 2) + 0.0612 * Cl_A + 0.7783
+#Aero_mass = 0.3745 * (Cl_A ** 2) + 0.0612 * Cl_A + 0.7783
 #######################################
 
 #GGV Mesh Parameters
@@ -33,21 +34,22 @@ sweep_range = {"body_slip": (-10 * np.pi / 180, 10 * np.pi / 180),
             "velocity" : (3, 30),
             "torque_request": (-1, 1),
             "is_left_diff_bias": (True, False)}
-mesh_size = 9 #Anything below 5 is troublesome, time to generate increases to the forth power of mesh size
+mesh_size = 13 #Anything below 5 is troublesome, time to generate increases to the forth power of mesh size
 
 #TODO: Get most up to date track (I believe Robert is working on it)
 endurance_track = engine.Track("racing_lines/en_mi_2019.csv", 1247.74)
 autocross_track = engine.Track("racing_lines/ax_mi_2019.csv", 50.008)
 skidpad_times = 5.0497
 accel_times = 4.004
+Drag_kWh = 0
 
 easy_driver = engine.Racecar(vehicles.Concept2023(motor_directory="engine/magic_moment_method/vehicle_params/Eff228.csv"))
-logger = Fuck_this_Logger.Logger()
-aero = Aero.Aerodynamics(vehicles.Concept2023(motor_directory="engine/magic_moment_method/vehicle_params/Eff228.csv"),logger)
+#logger = Fuck_this_Logger.Logger()
+#aero = Aero.Aerodynamics(vehicles.Concept2023(motor_directory="engine/magic_moment_method/vehicle_params/Eff228.csv"),logger)
 
 #TODO: Increase results to capture all even times and points in their specific columns, not the entire df
 results_df = pd.DataFrame(columns=["ClA","CdA","endurance_points","autocross_points","skidpad_points","accel_points",
-        "endurance_time","autocross_time","skidpad_time","accel_time", "endurance_results"])
+        "endurance_time","autocross_time","skidpad_time","accel_time", "drag_energy", "added_mass"])
 
 if __name__ == '__main__':
     for i in range(divisions):
@@ -55,12 +57,12 @@ if __name__ == '__main__':
         #TODO: Look into altering CL distribution to inflence CoP location
         easy_driver.params.ClA_tot = Cl_A[i]
         easy_driver.params.CdA_tot = Cd_A[i]
-        Drag_kWh = 0
+        Added_Mass = 11*0.45 + 15.770* 0.45 * Drag_kWh
 
         if easy_driver.params.ClA_tot == 0:
             easy_driver.params.mass_sprung -= 18
         else:
-            easy_driver.params.mass_sprung += 11*0.45 + 15.770* 0.45 * Drag_kWh #Added battery mass
+            easy_driver.params.mass_sprung += Added_Mass
 
         print("Testing CL_A: " + str(Cl_A[i]) + " and Cd_A: " + str(Cd_A[i]) )
         easy_driver.regenerate_GGV(sweep_range, mesh_size)
@@ -68,10 +70,10 @@ if __name__ == '__main__':
         results, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,skidpad_times, accel_times).run()
         df_endurance = results[0]
         df_endurance = df_endurance[["dist",'vel']]
-        df_endurance['Drag_kWh'] = easy_driver.params.CdA_tot * df_endurance['vel'] ** 2 * aero.__air_density() / 2 * df_endurance['dist']
 
-        Drag_kWh = df_endurance["Drag_kWh"].sum()/(times[0] * 15 *1000)
+        df_endurance['Drag_J'] = easy_driver.params.CdA_tot * df_endurance['vel'] ** 2 * 1.153 / 2 * df_endurance['dist']
+        Drag_kWh = (df_endurance["Drag_J"].sum()/(times[0] * 15 * 1000) * (times[0] * 15)/3600)
         print(times)
         print(points)
-        results_df.loc[i] = [Cl_A, Cd_A, points[0], points[1], points[2], points[3], times[0], times[1], times[2], times[3],results[0]]
+        results_df.loc[i] = [Cl_A[i], Cd_A[i], points[0], points[1], points[2], points[3], times[0], times[1], times[2], times[3], Drag_kWh, Added_Mass]
     results_df.to_csv("results/aero_sweep-easy_driver.csv")
