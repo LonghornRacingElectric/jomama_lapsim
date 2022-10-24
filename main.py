bb9f28f6f9dc21d27776d5efe6b753ec30ac61f0
kierancoz <kierancoz.w@gmail.com>
2022-10-24 13:02:30 -0500

    work

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import pandas as pd
 def main():
     # NOTE: this requires a GGV to be pre generated (second input)
     easy_driver = engine.Racecar(vehicles.Concept2023(motor_directory="engine/magic_moment_method/vehicle_params/Eff228.csv"))
-    endurance_track = engine.Track("racing_lines/en_mi_2019.csv", 1247.74)
+    #easy_driver.ggv = pd.read_csv("results/GGV.csv")
+    endurance_track = engine.Track("racing_lines/en_mi_2019.csv", 1247.74, track_type="endurance")
     autocross_track = engine.Track("racing_lines/ax_mi_2019.csv", 50.008)
     skidpad_times = 5.0497
     accel_times = 4.004
@@ -23,37 +24,44 @@ def main():
     ##########################
     ### Example: Sweeping! ###
 
-    # masses = np.array([400, 500, 600]) - 22*4 * .454
-    # results_df = pd.DataFrame(columns=["mass", "points", "endurance_time"])
-    # for index, mass in enumerate(masses):
-    #     easy_driver.params.mass_sprung = mass
-    #     easy_driver.regenerate_GGV(sweep_range, mesh_size)
+    #masses = np.array([400, 450, 500, 550, 600]) - 22*4 * .454
+    power_limit = [80000, 70000, 60000]
+    results_df = pd.DataFrame(columns=["power_limit", "points", "times", "endurance_battery_capacity"])
+    for index, p_l in enumerate(power_limit):
+        easy_driver.params.power_limit = p_l
+        easy_driver.regenerate_GGV(sweep_range, mesh_size)
 
-    #     # If a new racing line is needed (i.e. trackwidth changes), enable these lines
-    #     #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
-    #     #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
+        # If a new racing line is needed (i.e. trackwidth changes), enable these lines
+        #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
+        #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
 
-    #     _, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
-    #                         skidpad_times, accel_times).run()
-    #     print(times)
-    #     results_df.loc[index] = [mass, points, times[0]]
-    # results_df.to_csv("results/mass_sweep-easy_driver.csv")
+        p_l_results, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
+                            skidpad_times, accel_times).run()
+        energy = sum(p_l_results[0]["delta_t"] * p_l_results[0]["power_in"])
+        print(times)
+        print(points)
+        results_df.loc[index] = [p_l, points, times, energy]
+        p_l_results[0].to_csv(f"results/p_l_{str(p_l)}-endurance-easy_driver.csv")
+    results_df.to_csv("results/p_l_sweep-easy_driver.csv")
 
 
     ####################################
     ### Example: One-off simulations ###
 
-    easy_driver.ggv = pd.read_csv("results/GGV.csv")
-    #easy_driver.regenerate_GGV(sweep_range, mesh_size)
-    #easy_driver.save_ggv("results/GGV.csv")
-    #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
-    #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
-    results, points, times = engine.Competition(easy_driver, endurance_track, autocross_track,
-                           skidpad_times, accel_times).run()
-    results[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
-    results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
-    print(times)
-    print(points)
+    # easy_driver.ggv = pd.read_csv("results/GGV.csv")
+    # #easy_driver.regenerate_GGV(sweep_range, mesh_size)
+    # #easy_driver.save_ggv("results/GGV.csv")
+    # #endurance_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
+    # #autocross_track.regenerate_racing_line(easy_driver.params.front_trackwidth)
+    # comp = engine.Competition(easy_driver, endurance_track, autocross_track,
+    #                        skidpad_times, accel_times)
+    # results, points, times = comp.run()
+    # results[0].to_csv("results/endurance_michigan_2019-easy_driver.csv")
+    # results[1].to_csv("results/autocross_michigan_2019-easy_driver.csv")
+    # comp.autocross_sim.forward_sim_results.to_csv("results/forward-autocross_michigan_2019-easy_driver.csv")
+    # comp.autocross_sim.reverse_sim_results.to_csv("results/reverse-autocross_michigan_2019-easy_driver.csv")
+    # print(times)
+    # print(points)
 
 if __name__ == "__main__":
     main()
